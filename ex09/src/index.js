c5ec92454c061d07ceba842579b862aaa1cd3be5
vladimir <vladimir.obradovic.sa@hotmail.com>
2021-09-23 16:22:18 +0200

    correction

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,7 +1,7 @@
 function myFunction(myObj, checkProp) {
     // Only change code below this line
 
-    return myObj.hasOwnProperty(chechProp) ? myObj[checkProp] : "Not Found";
+    return myObj.hasOwnProperty(checkProp) ? myObj[checkProp] : "Not Found";
     
    
     // Only change code above this line
