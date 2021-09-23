bd5f41bb85319be2b8ce30abc85c4273ac582d1f
vladimir <vladimir.obradovic.sa@hotmail.com>
2021-09-23 09:54:23 +0200

    record change

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -12,18 +12,18 @@ function myFunction() {
             gold: true
         },
         002: {
-            artist: "Michael Jackson",
-            title: "Ghosts",
-            release_year: 1996,
+            artist: "Robbie Williams",
+            title: "Feel",
+            release_year: 2002,
             formats: {
                 1: "CD",
                 2: "8T",
                 3: "LP"
             },
-            gold: false
+            gold: true
         }
     };
     return myMusic;
 }
-myFunction()[1];
+myFunction()[2];
 module.exports = myFunction;
